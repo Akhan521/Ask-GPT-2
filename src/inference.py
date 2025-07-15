6783163172a3858de59b7f12eafb2d62adbcfb20
Aamir Khan <aamirksfg@gmail.com>
2025-07-14 22:49:29 -0700

    pushing inference script for text gen using finetuned model

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,4 @@
-from src.data_loader import load_data, get_tokenizer
+from src.data_loader import get_tokenizer
 from src.model import load_model
 from src.generate import generate
 import torch
@@ -7,15 +7,23 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # The model we will use.
-    model_name = "distilgpt2"
+    # Determine whether to use a fine-tuned model or the base model.
+    use_base = False             # Set this to 'True' if you want to use the base model.
+    use_local_finetuned = False  # Set this to 'True' if you want to load your locally fine-tuned model from the 'logs' directory.
+
+    # The model we will use (My fine-tuned model is saved in the Hugging Face Hub).
+    model_path = "distilgpt2" if use_base else "logs" if use_local_finetuned else "akhan365/distilgpt2-finetuned-on-guanaco-for-qa"
 
     # Load the tokenizer and model.
-    tokenizer = get_tokenizer(model_name)
-    model = load_model(model_name)
+    print(f"\nLoading model and tokenizer from {model_path}...")
+    tokenizer = get_tokenizer(model_path)
+    model = load_model(model_path)
 
-    prompt = "how are you?"
+    # Play around with the prompt you want to generate text for.
+    prompt = "how are you"
 
-    print()
+    print(f'\nGenerating text using {'fine-tuned' if not use_base else 'base'} model...')
+    print('=' * 50)
+    print(f'Prompt: "{prompt}"\n')
     generated_text = generate(prompt, model, tokenizer, device)
-    print()
+    print(f'\nGenerated text: "{generated_text}"\n')
